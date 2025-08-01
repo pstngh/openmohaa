ioquake/ioq3

Replace USE_LOCAL_HEADERS with USE_INTERNAL_*

code/renderercommon/qgl.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef __QGL_H__
 #define __QGL_H__
 
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL
 #	include "SDL_opengl.h"
 #else
 #	include <SDL_opengl.h>

code/renderergl2/tr_extensions.c
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 // tr_extensions.c - extensions needed by the renderer not in sdl_glimp.c
 
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL
 #	include "SDL.h"
 #else
 #	include <SDL.h>

code/sdl/sdl_snd.c
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL
 #	include "SDL.h"
 #else
 #	include <SDL.h>

code/sys/sys_loadlib.h
@@ -35,7 +35,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #		define Sys_LibraryError() dlerror()
 #	endif
 #else
-#	ifdef USE_LOCAL_HEADERS
+#	ifdef USE_INTERNAL_SDL
 #		include "SDL.h"
 #		include "SDL_loadso.h"
 #	else

code/sys/sys_local.h
@@ -24,7 +24,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include "../qcommon/qcommon.h"
 
 #ifndef DEDICATED
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL
 #	include "SDL_version.h"
 #else
 #	include <SDL_version.h>

code/sys/sys_main.c
@@ -32,7 +32,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <errno.h>
 
 #ifndef DEDICATED
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL
 #	include "SDL.h"
 #	include "SDL_cpuinfo.h"
 #else

pstngh/openmohaa

Co-authored-by: smallmodel <15067410+smallmodel@users.noreply.github.com>

code/renderercommon/qgl.h
@@ -26,7 +26,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef __QGL_H__
 #define __QGL_H__
 
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL_HEADERS
 #	include "SDL_opengl.h"
 #else
 #	include <SDL_opengl.h>

code/renderergl2/tr_extensions.c
@@ -21,7 +21,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 // tr_extensions.c - extensions needed by the renderer not in sdl_glimp.c
 
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL_HEADERS
 #	include "SDL.h"
 #else
 #	include <SDL.h>

code/sdl/sdl_snd.c
@@ -23,7 +23,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #include <stdlib.h>
 #include <stdio.h>
 
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL_HEADERS
 #	include "SDL.h"
 #else
 #	include <SDL.h>

code/sys/sys_loadlib.h
@@ -35,7 +35,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #		define Sys_LibraryError() dlerror()
 #	endif
 #else
-#	ifdef USE_LOCAL_HEADERS
+#	ifdef USE_INTERNAL_SDL_HEADERS
 #		include "SDL.h"
 #		include "SDL_loadso.h"
 #	else

code/sys/sys_local.h
@@ -28,7 +28,7 @@ extern "C" {
 #endif
 
 #ifndef DEDICATED
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL_HEADERS
 #	include "SDL_version.h"
 #else
 #	include <SDL_version.h>

code/sys/sys_main.c
@@ -36,7 +36,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #endif
 
 #ifndef DEDICATED
-#ifdef USE_LOCAL_HEADERS
+#ifdef USE_INTERNAL_SDL_HEADERS
 #	include "SDL.h"
 #	include "SDL_cpuinfo.h"
 #else